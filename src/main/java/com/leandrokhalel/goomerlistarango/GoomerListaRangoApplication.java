12ed1a0ac43ffc3ef760d8fb6539f03c1905e840
Leandro Khalel <leandrokhalelpimentel@gmail.com>
2024-03-15 00:01:44 -0300

    Projeto iniciado

---
+++ src/main/java/com/leandrokhalel/goomerlistarango/GoomerListaRangoApplication.java
@@ -0,0 +1,13 @@
+package com.leandrokhalel.goomerlistarango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoomerListaRangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoomerListaRangoApplication.class, args);
+	}
+
+}
